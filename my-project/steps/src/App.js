6812648bf3aa1ea25bb793ede40817c5eb364cc9
yy <30777685+yy-personal@users.noreply.github.com>
2025-07-16 23:59:43 +0800

    Add step navigation UI to App component
    Replaces the static 'Hello' message with a step-based UI. Implements navigation between steps using Previous and Next buttons, displays step numbers and messages, and manages state with useState.

--- my-project/steps/src/App.js
+++ my-project/steps/src/App.js
@@ -1,3 +1,50 @@
-export default function App(){
-  return <div>Hello</div>
+import { useState } from "react";
+
+const messages = ["Learn React", "Apply for jobs", "Invest income"];
+
+export default function App() {
+	// const array = useState(1)
+	// console.log(array);
+	const[step, setStep] = useState(1) // only use this to set state
+
+	function handlePrevious(){
+		if(step > 1){
+			setStep(step - 1);
+		}
+		
+	}
+	function handleNext() {
+		if (step <3 ) {
+			setStep(step + 1);
+		}
+	}
+	
+	return (
+		<div className="steps">
+			<div className="numbers">
+				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
+				<div className={`${step >= 2 ? "active" : ""}`}>2</div>
+				<div className={`${step >= 3 ? "active" : ""}`}>3</div>
+			</div>
+
+			<p className="message">
+				Step {step}:{messages[step - 1]}
+			</p>
+
+			<div className="buttons">
+				<button
+					style={{ backgroundColor: "#7950f2", color: "white" }}
+					onClick={handlePrevious}
+				>
+					Previous
+				</button>
+				<button
+					style={{ backgroundColor: "purple", color: "white" }}
+					onClick={handleNext}
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
