010e252bf073753fe75e6ab2e4715a9114c7be2b
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2025-12-29 16:50:26 +0800

    replace final files with original,initiate pizza menu starter

---
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return <h1>Hello</h1>
+}
+
+//React 18
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
