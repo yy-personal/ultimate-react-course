8ed6d84d66210659ac33711b517b7429dca2642d
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2025-12-31 11:50:20 +0800

    57

--- my-project/steps/src/App.js
+++ my-project/steps/src/App.js
@@ -1,74 +1,25 @@
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const messages = ["Learn React", "Apply for jobs", "Invest income"];
-
-export default function App() {
-	// const array = useState(1)
-	// console.log(array);
-	const [step, setStep] = useState(1); // only use this to set state
-	const [isOpen, setIsOpen] = useState(true);
-
-	function handlePrevious() {
-		if (step > 1) {
-			setStep((currentStep) => currentStep - 1);
-			// setStep(step - 1);
-		}
-	}
-	function handleNext() {
-		if (step < 3) {
-			setStep((currentStep) => currentStep + 1);
-			// setStep(step + 1);
-		}
-	}
-	function handleToggle() {
-		setIsOpen((currentIsOpen) => !currentIsOpen);
-		// setStep(1);
-	}
-
-	
-
-	return (
-		<>
-			{/* <button className="close" onClick={() => setIsOpen(!isOpen)}>
-				<b>x</b>
-			</button> */}
-			<button className="close" onClick={handleToggle}>
-				<b>x</b>
-			</button>
-			{isOpen && (
-				<div className="steps">
-					<div className="numbers">
-						<div className={`${step >= 1 ? "active" : ""}`}>1</div>
-						<div className={`${step >= 2 ? "active" : ""}`}>2</div>
-						<div className={`${step >= 3 ? "active" : ""}`}>3</div>
-					</div>
-
-					<p className="message">
-						Step {step}:{messages[step - 1]}
-					</p>
-
-					<div className="buttons">
-						<button
-							style={{
-								backgroundColor: "#7950f2",
-								color: "white",
-							}}
-							onClick={handlePrevious}
-						>
-							Previous
-						</button>
-						<button
-							style={{
-								backgroundColor: "purple",
-								color: "white",
-							}}
-							onClick={handleNext}
-						>
-							Next
-						</button>
-					</div>
-				</div>
-			)}
-		</>
-	);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
