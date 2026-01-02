d6d37765929546c92ad977d5a1e31b771f9e2023
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-02 14:56:25 +0800

    finish challenge '68'

--- my-project/react-challenge-date-counter/src/App.js
+++ my-project/react-challenge-date-counter/src/App.js
@@ -11,20 +11,48 @@ export default function App() {
 
 function Counter() {
   const [step, setStep] = useState(1) // step will be added into counter
+  const [counter, setCounter] = useState(1) // counter to cumulative value from step, used as days too
 
-  const [counter,setCounter]=useState(1) // counter to cumulative value from step
+
+  function increaseStep() {
+    setStep((s) => s + 1);
+  }
+  function decreaseStep() {
+    if (step > 1) {
+      setStep((currentStep) => currentStep - 1);
+    }
+  }
+  function increaseCounter() {
+    setCounter((c) => c + step);
+  }
+  function decreaseCounter() {
+    setCounter((c) => c - step);
+  }
+
+  const date = new Date()
+  date.setDate(date.getDate() + counter)
 
   return (
-  <div className='counter-container'>
-    <div className='counter-row'>
-      <button>-</button><p>Step:</p><button>+</button>
-    </div>
-    <div className='counter-row'>
-      <button>-</button><p>Counter:</p><button>+</button>
-    </div>
-      <h3>x days from now is (Day of week)( Month date year)</h3>
+    <div className='counter-container'>
+      <div className='counter-row'>
+        <button onClick={decreaseStep}>-</button>
+        <p>Step:{step}</p>
+        <button onClick={increaseStep}>+</button>
+      </div>
+      <div className='counter-row'>
+        <button onClick={decreaseCounter}>-</button><p>Counter:{counter}</p><button onClick={increaseCounter}>+</button>
+      </div>
 
-  </div>
+      <h3>
+        <span>{counter === 0 
+        ? "Today is" 
+        : counter > 0 
+        ? `${counter} days from now is` 
+        : `${Math.abs(counter)} days ago was`}</span>
+        <span> {date.toDateString()}</span>
+      </h3>
+
+    </div>
 
   )
 }
