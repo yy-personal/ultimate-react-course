cafa846d408c62480a20497c8f01e0af9a60e172
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-02 11:49:45 +0800

    68 .01

--- my-project/react-challenge-date-counter/src/App.js
+++ my-project/react-challenge-date-counter/src/App.js
@@ -1,6 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  return 
+  return (
+    <div className='App'>
+      <Counter />
+    </div>
+  )
 }
+
+function Counter() {
+  const [step, setStep] = useState(1) // step will be added into counter
+
+  const [counter,setCounter]=useState(1) // counter to cumulative value from step
+
+  return (
+  <div className='counter-container'>
+    <div className='counter-row'>
+      <button>-</button><p>Step:</p><button>+</button>
+    </div>
+    <div className='counter-row'>
+      <button>-</button><p>Counter:</p><button>+</button>
+    </div>
+      <h3>x days from now is (Day of week)( Month date year)</h3>
+
+  </div>
+
+  )
+}
+
+
 
