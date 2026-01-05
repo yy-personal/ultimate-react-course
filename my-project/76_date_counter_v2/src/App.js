7e053a0df0cc1a649537d9447953a3a2e3a0af21
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-05 11:53:32 +0800

    76 completed

--- my-project/76_date_counter_v2/src/App.js
+++ my-project/76_date_counter_v2/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [date, setDate] = useState(1);
+	return (
+		<div className="App">
+			<Counter />
+		</div>
+	);
 }
 
-export default App;
+function Counter() {
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
+	const date = new Date("june 21 2024");
+	date.setDate(date.getDate() + count);
+
+	function resetCount(){
+		setCount(0)
+	}
+	return (
+		<div>
+			<>
+				Steps: <input type="range" min="0" max="10" value={step} onChange={(event)=>
+					setStep(Number(event.target.value))
+				}/>
+			</>
+			<br />
+			<>				
+				<span>Step: {step}</span>
+			</>
+			<br />
+			<>
+				
+				<button onClick={() => setCount((c) => c - step)}>-</button>
+				<input type="number" value={count} onChange={(event)=>
+				setCount(Number(event.target.value))
+				}/>
+				<button onClick={() => setCount((c) => c + step)}>+</button>
+			</>
+			<p>
+				<span>
+					{count === 0
+						? "Today is "
+						: count > 0
+						? `${count} days from today is `
+						: `${Math.abs(count)} days ago was `}
+				</span>
+				<span>{date.toDateString()}</span>
+			</p>
+			{count !== 0 && <button onClick={resetCount}>Reset</button>}
+		</div>
+	);
+}
+
+function CurrentDate() {}
