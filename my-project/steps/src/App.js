2583db6f85857ca7ce1fcbc016a7024cd381f6ab
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-06 11:45:41 +0800

    91 children props reusability

--- my-project/steps/src/App.js
+++ my-project/steps/src/App.js
@@ -7,9 +7,33 @@ const messages = [
 ];
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(true);
 
-  const [step, setStep] = useState(1)
-  const [isOpen, setIsOpen] = useState(true)
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen &&
+        <>
+          (<Step messages={messages} />
+          <StepMessage step={1}>
+            <p>Pass in content </p>
+            <p>First</p>
+          </StepMessage>
+          <StepMessage step={2}>
+            <p>Read child prop </p>
+            <p>Second</p>
+          </StepMessage>)
+        </>
+      }
+    </>
+  );
+}
+
+
+function Step({ messages }) {
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
     if (step > 1) {
@@ -24,29 +48,49 @@ export default function App() {
   }
 
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>&times;</button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers" >
-            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-          </div>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
 
-          <p className="message">Step {step}: {messages[step - 1]}</p>
+      <StepMessage step={step}>
+        {messages[step - 1]}
+        <div className="buttons">
+          <Button bgColor='#e7e7e7' textColor='#333' onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>Learn how
+          </Button>
+        </div>
 
-          <div className="buttons">
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}><span>👈</span>Previous</Button>
+      </StepMessage>
 
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}><span>Previous👉</span></Button>
-          </div>
-        </div>)}
-    </>)
+      <div className="buttons">
+        <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}>
+          <span>👈</span>Previous
+        </Button>
+        <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}>
+          <span>👉</span>Next
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-function Button({ bgColor, textColor, onClick, children}) {
-  return (<button
-    style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>{children}
-  </button>)
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      Step {step}: {children}
+    </p>
+  );
+}
+
+function Button({ bgColor, textColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
