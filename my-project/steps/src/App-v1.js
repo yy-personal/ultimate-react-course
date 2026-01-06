281f272e53ee4413fcf27d8aecb3e3c869290c15
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-06 11:22:41 +0800

    90 children prop

--- my-project/steps/src/App-v1.js
+++ my-project/steps/src/App-v1.js
@@ -13,19 +13,19 @@ export default function App() {
 
   function handlePrevious() {
     if (step > 1) {
-      setStep((currentStep) => currentStep - 1);
+      setStep((currentStep)=>currentStep-1);
     }
   }
 
   function handleNext() {
     if (step < 3) {
-      setStep((s) => s + 1);
+      setStep((s)=>s+1);
     }
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>&times;</button>
+      <button className="close" onClick={()=>setIsOpen((isOpen)=>!isOpen)}>&times;</button>
       {isOpen && (
         <div className="steps">
           <div className="numbers" >
@@ -37,16 +37,12 @@ export default function App() {
           <p className="message">Step {step}: {messages[step - 1]}</p>
 
           <div className="buttons">
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}><span>👈</span>Previous</Button>
-
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}><span>Previous👉</span></Button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}>Previous</button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >Next</button>
           </div>
         </div>)}
     </>)
-}
-
-function Button({ bgColor, textColor, onClick, children}) {
-  return (<button
-    style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>{children}
-  </button>)
 }
