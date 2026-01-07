0ee3fa6a3e685b5ab63adad398262a49e90774ff
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-07 14:53:31 +0800

    103 feat(app): update split bill logic and enable balance updates
    Added handleSplitBill to update friends' balances when a bill is split and passed the handler to FormSplitBill. FormSplitBill now calls onSplitBill with the correct amount on submit, improving bill-splitting functionality and setting the selected friend to null afterwards.

--- my-project/eat-n-split/src/App.jsx
+++ my-project/eat-n-split/src/App.jsx
@@ -43,6 +43,18 @@ export default function App() {
 		setShowAddFriend(false);
 	}
 
+	function handleSplitBill(value) {
+		setFriends((friends) =>
+			friends.map((friend) =>
+				friend.id === selectedFriend.id
+					? { ...friend, balance: friend.balance + value }
+					: friend
+			)
+		);
+		console.log(value);
+		setSelectedFriend(null);
+	}
+
 	return (
 		<div className="app">
 			<div className="sidebar">
@@ -59,7 +71,10 @@ export default function App() {
 				</Button>
 			</div>
 			{selectedFriend && (
-				<FormSplitBill selectedFriend={selectedFriend} />
+				<FormSplitBill
+					selectedFriend={selectedFriend}
+					onSplitBill={handleSplitBill}
+				/>
 			)}
 		</div>
 	);
@@ -158,13 +173,21 @@ function FormAddFriend({ onAddFriend }) {
 	);
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
 	const [bill, setBill] = useState("");
 	const [paidByUser, setPaidByUser] = useState("");
 	const paidByFriend = bill ? bill - paidByUser : "";
 	const [whoIsPaying, setWhoIsPaying] = useState("user");
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (!bill || !paidByUser) {
+			return;
+		}
+		onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+	}
+
 	return (
-		<form className="form-split-bill">
+		<form className="form-split-bill" onSubmit={handleSubmit}>
 			<h2>Split a bill with {selectedFriend.name}</h2>
 			<label>Bill value</label>
 			<input
