e321cbd96cf640434d297bc68ea959465875a0cf
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-08 16:54:34 +0800

    119 completed with review
    To review:
    -messages description logic

--- my-project/usepopcorn/src/main.jsx
+++ my-project/usepopcorn/src/main.jsx
@@ -1,14 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
 // import './index.css';
 // import App from './App.jsx'
 
-import StarRating from './StarRating.jsx';
-
+import StarRating from "./StarRating.jsx";
+function Test() {
+  const [movieRating, setMovieRating]=useState(0)
+  
+	return (
+		<div>
+			<StarRating
+				maxRating={10}
+				color="blue"
+				onSetMovieRating={setMovieRating}
+			/>
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		{/* <App /> */}
-		<StarRating maxRating={5} />
-
+		<StarRating
+			maxRating={5}
+			messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+		/>
+		<StarRating
+			maxRating={10}
+			color="red"
+			size={24}
+			className="test"
+			defaultRating={3}
+		/>
+    <Test/>
 	</StrictMode>
 );
