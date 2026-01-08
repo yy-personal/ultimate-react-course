e052fb3a6bdd22fe6e2b2002ab352b575ff5b330
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-08 11:26:56 +0800

    117 temp rating

--- my-project/usepopcorn/src/StarRating.jsx
+++ my-project/usepopcorn/src/StarRating.jsx
@@ -16,21 +16,29 @@ const textStyle = {
 	margin: "0",
 };
 export default function StarRating({ maxRating = 5 }) {
-	const [rating, setRating] = useState(1);
+	const [rating, setRating] = useState(0);
+	const [tempRating, setTempRating] = useState(0);
+
 	function handleRating(rating) {
 		setRating(rating);
 	}
+
 	return (
 		<div style={containerStyle}>
 			<div style={starContainerStyle}>
 				{Array.from({ length: maxRating }, (_, i) => (
-					<Star 
-                    key={i} 
-                    onRate={() => handleRating(i + 1)} 
-                    full={rating >= i+1}/>
+					<Star
+						key={i}
+						onRate={() => handleRating(i + 1)}
+						onHoverIn={() => setTempRating(i + 1)}
+						onHoverOut={() => setTempRating(0)}
+						full={
+							tempRating ? tempRating >= i + 1 : rating >= i + 1
+						}
+					/>
 				))}
 			</div>
-			<div style={textStyle}>{rating || ""}</div>{" "}
+			<div style={textStyle}>{tempRating || rating || ""}</div>
 			{/* Falsy value then goes to '' */}
 		</div>
 	);
@@ -41,9 +49,15 @@ const starStyle = {
 	display: "block",
 	cursor: "pointer",
 };
-function Star({ onRate , full}) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
 	return (
-		<span role="button" style={starStyle} onClick={onRate}>
+		<span
+			role="button"
+			style={starStyle}
+			onClick={onRate}
+			onMouseEnter={() => onHoverIn()}
+			onMouseLeave={() => onHoverOut()}
+		>
 			{full ? (
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
