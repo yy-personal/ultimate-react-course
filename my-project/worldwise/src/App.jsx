87795ede7ee02afad627ecd1da200b5168523b9a
Yin Yao Lim <30777685+yy-personal@users.noreply.github.com>
2026-01-11 23:50:21 +0800

    209 using css modules
    Integrated react-router-dom for client-side routing and created initial pages (Homepage, Product, Pricing, AppLayout, PageNotFound) and navigation components (AppNav, PageNav). Added global and module CSS files for styling, updated main entry to include global styles, and included starter assets and data for further development.

--- my-project/worldwise/src/App.jsx
+++ my-project/worldwise/src/App.jsx
@@ -1,13 +1,26 @@
-// import { useState } from 'react'
-// import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
+	return (
+		<div>
+			{/* The part of page that will always stays the same */}
+			<h1>Hello</h1>
 
-  return (
-      <div>
-        WorldWise
-      </div>
-  )
+			<BrowserRouter>
+				<Routes>
+					<Route path="product" element={<Product />} />
+					<Route path="pricing" element={<Pricing />} />
+					<Route path="app" element={<AppLayout />} />
+					<Route path="/" element={<Homepage />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
-
-export default App
+export default App;
---
+++ my-project/worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+	return (
+		<div>
+			<PageNav />
+            <h1>Not Found :(</h1>
+			
+		</div>
+	);
+}
+export default PageNotFound;
