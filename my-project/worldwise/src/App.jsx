454237856e79784282f0a3accaceb17b6f6d57dd
Yin Yao Lim <30777685+yy-personal@users.noreply.github.com>
2026-01-12 22:56:29 +0800

    210 Building the Pages

--- my-project/worldwise/src/App.jsx
+++ my-project/worldwise/src/App.jsx
@@ -4,19 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
 	return (
 		<div>
 			{/* The part of page that will always stays the same */}
-			<h1>Hello</h1>
+			<h1>Hello TOP of Page at APP</h1>
 
 			<BrowserRouter>
 				<Routes>
+					<Route path="/" element={<Homepage />} />
 					<Route path="product" element={<Product />} />
 					<Route path="pricing" element={<Pricing />} />
 					<Route path="app" element={<AppLayout />} />
-					<Route path="/" element={<Homepage />} />
+					<Route path="/login" element={<Login />} />
+
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
--- my-project/worldwise/src/pages/Homepage.jsx
+++ my-project/worldwise/src/pages/Homepage.jsx
@@ -1,16 +1,27 @@
+import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav"
 import { Link } from "react-router-dom";
-import PageNav from "../components/PageNav";
-import AppNav from "../components/AppNav";
 
-function Homepage() {
-	return (
-		<div>
-			<PageNav />
-            <AppNav/>
-			<h1>WorldWise</h1>
-			<Link to="/app">Go to the app </Link>
-            
-		</div>
-	);
+export default function Homepage() {
+
+  return (
+    <main className={styles.homepage}>
+      <PageNav/>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <button>
+          <Link to="/app" className="cta">Start Tracking Now</Link>
+        </button>
+      </section>
+    </main>
+  );
 }
-export default Homepage;
