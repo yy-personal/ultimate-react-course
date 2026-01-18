1ea80ff221ea87a354a4e02c6255d0ffee25d194
Yin Yao Lim <30777685+yy-personal@users.noreply.github.com>
2026-01-18 21:58:03 +0800

    217. Reading and Setting a Query String useSearchParams()

--- my-project/worldwise/src/components/City.jsx
+++ my-project/worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -14,6 +14,10 @@ function City() {
   const {id} = useParams();
   console.log(id)
 
+const [searchParams, setSearchParams] = useSearchParams();
+const lat = searchParams.get("lat");
+const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -23,7 +27,7 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return (<h1>City {id}</h1>
+  return (<h1>City {id}<p>Position: {lng} {lat}</p></h1>
 
   )
   // return (
