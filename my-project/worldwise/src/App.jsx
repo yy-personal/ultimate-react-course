3d8ae95de116fbd4583d8722a3356322a9e92f41
Yin Yao Lim <30777685+yy-personal@users.noreply.github.com>
2026-01-19 15:46:41 +0800

    227. Back to "WorldWise": Creating a CitiesContext

--- my-project/worldwise/src/App.jsx
+++ my-project/worldwise/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState, useNavigate } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -10,31 +9,11 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:9001";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-	const [cities, setCities] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(function () {
-		async function fetchCities() {
-			try {
-				setIsLoading(true);
-				const res = await fetch(`${BASE_URL}/cities`);
-				const data = await res.json();
-				setCities(data);
-			} catch {
-				alert("There was an error loading data");
-			} finally {
-				setIsLoading(false);
-			}
-		}
-		fetchCities();
-	}, []);
-
 	return (
-		<div>
+		<CitiesProvider>
 			{/* The part of page that will always stays the same */}
 			<h1>Hello TOP of Page at APP</h1>
 
@@ -45,25 +24,15 @@ function App() {
 					<Route path="pricing" element={<Pricing />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="app" element={<AppLayout />}>
-						<Route index element={<Navigate replace to="cities" />}></Route>
-						<Route path="cities/:id" element={<City />} />
 						<Route
-							path="cities"
-							element={
-								<CityList
-									cities={cities}
-									isLoading={isLoading}
-								/>
-							}
+							index
+							element={<Navigate replace to="cities" />}
 						></Route>
+						<Route path="cities/:id" element={<City />} />
+						<Route path="cities" element={<CityList />}></Route>
 						<Route
 							path="countries"
-							element={
-								<CountryList
-									cities={cities}
-									isLoading={isLoading}
-								/>
-							}
+							element={<CountryList />}
 						></Route>
 						<Route path="form" element={<Form />}></Route>
 					</Route>
@@ -71,7 +40,7 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</div>
+		</CitiesProvider>
 	);
 }
 export default App;
