5796385a7d651f2b56ac655c627c7adba27821a2
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-21 17:50:02 +0800

    301. Using CSS Grid in progress
    top and bottom distribution using rows logic

--- my-project/fast-react-pizza/src/ui/AppLayout.jsx
+++ my-project/fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,14 +1,14 @@
-import Header from './Header';
-import Loader from './Loader';
-import CartOverview from '../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
+import Header from "./Header";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div>
+    <div className="grid h-[500px] grid-rows-3 bg-red-400">
       {isLoading && <Loader />}
 
       <Header />
