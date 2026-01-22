ce17f238d086d466aa6108a8bc02028fdb3e6669
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-22 17:54:17 +0800

    308

--- my-project/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ my-project/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,9 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-import Button from '../../ui/Button';
-import DeleteItem from '../cart/DeleteItem';
-import UpdateItemQuantity from '../cart/UpdateItemQuantity';
-import { formatCurrency } from '../../utils/helpers';
-import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../../ui/Button";
+import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
+import { formatCurrency } from "../../utils/helpers";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -25,16 +25,24 @@ function MenuItem({ pizza }) {
   }
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 w-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex flex-col grow mt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex  item-center justify-between" >
           {!soldOut ? (
-            <p>{formatCurrency(unitPrice)}</p>
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p>Sold out</p>
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
           )}
 
           {isInCart && (
--- my-project/fast-react-pizza/src/ui/Button.jsx
+++ my-project/fast-react-pizza/src/ui/Button.jsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type, onClick }) {
-  const className =
-  "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-slate-50 disabled:cursor-not-allowed disabled:bg-slate-600";
+  const base =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-slate-50 disabled:cursor-not-allowed disabled:bg-slate-600";
 
-  if (to) return <Link className={className} to={to}>{children}</Link>;
+  const styles = {
+    primary: base + " px:4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
+
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
 
   if (onClick)
     return (
-      <button onClick={onClick} disabled={disabled}>
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
         {children}
       </button>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
