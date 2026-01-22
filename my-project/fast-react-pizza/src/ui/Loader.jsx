887070ae82f10273831acbfbce0ddc1fc45dcea4
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-01-22 15:52:32 +0800

    306 - Absolute positioning(loading)

--- my-project/fast-react-pizza/src/ui/Loader.jsx
+++ my-project/fast-react-pizza/src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div>
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex justify-center items-center">
       <div className="loader"></div>
     </div>
   );
